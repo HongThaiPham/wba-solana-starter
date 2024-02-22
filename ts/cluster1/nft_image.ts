b6f4b730794a3eec3ca501b8dfca1aa5b4c478cf
LeoPham <hongthaipro@gmail.com>
2024-02-22 23:04:33 +0700

    Done challenge 3

--- ts/cluster1/nft_image.ts
+++ ts/cluster1/nft_image.ts
@@ -1,26 +1,43 @@
-import wallet from "../wba-wallet.json"
-import { createUmi } from "@metaplex-foundation/umi-bundle-defaults"
-import { createGenericFile, createSignerFromKeypair, signerIdentity } from "@metaplex-foundation/umi"
-import { createBundlrUploader } from "@metaplex-foundation/umi-uploader-bundlr"
-import { readFile } from "fs/promises"
+import wallet from "../wba-wallet.json";
+import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
+import {
+  createGenericFile,
+  createSignerFromKeypair,
+  signerIdentity,
+} from "@metaplex-foundation/umi";
+import { createBundlrUploader } from "@metaplex-foundation/umi-uploader-bundlr";
+import { irysUploader } from "@metaplex-foundation/umi-uploader-irys";
+import { readFile } from "fs/promises";
 
 // Create a devnet connection
-const umi = createUmi('https://api.devnet.solana.com');
+const umi = createUmi("https://api.devnet.solana.com");
 const bundlrUploader = createBundlrUploader(umi);
 
 let keypair = umi.eddsa.createKeypairFromSecretKey(new Uint8Array(wallet));
 const signer = createSignerFromKeypair(umi, keypair);
 
 umi.use(signerIdentity(signer));
-
+umi.use(irysUploader());
 (async () => {
-    try {
-        // const image = ???
+  try {
+    const image = await readFile(
+      "/home/leo/solana/wba/wba-solana-starter/ts/cluster1/images/generug.png"
+    );
 
-        // const [myUri] = ??? 
-        // console.log("Your image URI: ", myUri);
-    }
-    catch(error) {
-        console.log("Oops.. Something went wrong", error);
-    }
+    const [myUri] = await umi.uploader.upload([
+      createGenericFile(image, "generug", {
+        contentType: "image/png",
+      }),
+    ]);
+
+    // const [myUri] = await bundlrUploader.upload([
+    //   createGenericFile(image, "generug.png", {
+    //     contentType: "image/png",
+    //   }),
+    // ]);
+
+    console.log("Your image URI: ", myUri);
+  } catch (error) {
+    console.log("Oops.. Something went wrong", error);
+  }
 })();
